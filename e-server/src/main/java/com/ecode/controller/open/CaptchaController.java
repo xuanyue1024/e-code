67fc2e9b9843eff91e59012e2d8e050540404b9d
竹林听雨 <xuanyue2003@foxmail.com>
2024-12-30 20:18:22 +0800

    feat: oss文件上传

--- e-server/src/main/java/com/ecode/controller/open/CaptchaController.java
+++ e-server/src/main/java/com/ecode/controller/open/CaptchaController.java
@@ -1,12 +1,9 @@
 package com.ecode.controller.open;
 
-import com.ecode.result.Result;
 import com.ecode.service.CaptchaService;
 import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,17 +16,18 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RequestMapping("/open/captcha")
 @RestController
+@Deprecated//废弃
 @Api(tags = "验证码")
 public class CaptchaController {
 
     @Autowired
     private CaptchaService captchaService;
 
-    @GetMapping("/getCaptcha")
+    /*@GetMapping("/getCaptcha")
     @ApiOperation("发送验证码")
     public Result sendCaptcha(String email){
         captchaService.sendCaptcha(email);
         return Result.success();
-    }
+    }*/
 
 }
---
+++ e-server/src/main/java/com/ecode/controller/open/CommonController.java
@@ -0,0 +1,68 @@
+package com.ecode.controller.open;
+
+import com.ecode.constant.MessageConstant;
+import com.ecode.result.Result;
+import com.ecode.service.CaptchaService;
+import com.ecode.utils.AliOssUtil;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.UUID;
+
+/**
+ * 通用接口
+ */
+@RestController
+@RequestMapping("/open")
+@Api(tags = "开放接口")
+@Slf4j
+public class CommonController {
+
+    @Autowired
+    private AliOssUtil aliOssUtil;
+    @Autowired
+    private CaptchaService captchaService;
+
+    /**
+     * 文件上传
+     * @param file
+     * @return
+     */
+    @PostMapping("/upload")
+    @ApiOperation(value = "文件上传",notes = "data值为上传返回的链接")
+    public Result<String> upload(MultipartFile file){
+        log.info("文件上传：{}",file);
+
+        try {
+            //原始文件名
+            String originalFilename = file.getOriginalFilename();
+            //截取原始文件名的后缀   dfdfdf.png
+            String extension = originalFilename.substring(originalFilename.lastIndexOf("."));
+            //构造新文件名称
+            String objectName = UUID.randomUUID().toString() + extension;
+
+            //文件的请求路径
+            String filePath = aliOssUtil.upload(file.getBytes(), objectName);
+            return Result.success(filePath);
+        } catch (IOException e) {
+            log.error("文件上传失败：{}", e);
+        }
+
+        return Result.error(MessageConstant.UPLOAD_FAILED);
+    }
+
+    @GetMapping("/captcha/getCaptcha")
+    @ApiOperation("发送验证码")
+    public Result sendCaptcha(String email){
+        captchaService.sendCaptcha(email);
+        return Result.success();
+    }
+}
