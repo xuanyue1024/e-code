5175250c97fea4a26a04573f3cace789999bc01d
郭伟 <13639607+wei_flzxsqc@user.noreply.gitee.com>
2025-04-22 14:34:59 +0800

    feat：学生代码提交历史记录功能开发

--- e-pojo/src/main/java/com/ecode/entity/ClassScore.java
+++ e-pojo/src/main/java/com/ecode/entity/ClassScore.java
@@ -4,13 +4,15 @@ import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
+import com.baomidou.mybatisplus.extension.handlers.JacksonTypeHandler;
+import com.ecode.entity.po.CodeSubmission;
 import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.Builder;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
+import lombok.*;
 import lombok.experimental.Accessors;
 
+import java.io.Serial;
 import java.io.Serializable;
+import java.util.List;
 
 /**
  * <p>
@@ -24,10 +26,13 @@ import java.io.Serializable;
 @EqualsAndHashCode(callSuper = false)
 @Accessors(chain = true)
 @Builder
-@TableName("class_score")
+@TableName(value = "class_score", autoResultMap = true)
 @Schema(description="ClassScore对象")
+@AllArgsConstructor
+@NoArgsConstructor
 public class ClassScore implements Serializable {
 
+    @Serial
     private static final long serialVersionUID = 1L;
 
     @Schema(description = "班级学生题目得分id")
@@ -46,7 +51,6 @@ public class ClassScore implements Serializable {
     @TableField("score")
     private Integer score;
 
-
     @Schema(description = "提交次数")
     @TableField("submit_number")
     private Integer submitNumber;
@@ -54,4 +58,8 @@ public class ClassScore implements Serializable {
     @Schema(description = "通过次数")
     @TableField("pass_number")
     private Integer passNumber;
+
+    @Schema(description = "代码提交记录")
+    @TableField(value = "code_submission", typeHandler = JacksonTypeHandler.class)
+    private List<CodeSubmission> codeSubmission;
 }
