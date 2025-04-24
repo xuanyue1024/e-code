681f93232fe45cbaaf40ec9112c06b2cc71a800a
竹林听雨 <xuanyue2003@foxmail.com>
2025-04-24 18:01:57 +0800

    perf: redis ai会话持久化记忆

--- e-server/src/main/java/com/ecode/config/AiClientConfig.java
+++ e-server/src/main/java/com/ecode/config/AiClientConfig.java
@@ -8,7 +8,6 @@ import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
-import org.springframework.ai.chat.memory.InMemoryChatMemory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -55,9 +54,4 @@ public class AiClientConfig {
                 .build();
     }
 
-
-    @Bean
-    public ChatMemory chatMemory() {
-        return new InMemoryChatMemory();
-    }
 }
