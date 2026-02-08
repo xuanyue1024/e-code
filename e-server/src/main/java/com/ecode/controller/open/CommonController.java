2910740ccc4448786752bb41cf42a8dd25c11550
竹林听雨 <xuanyue2003@foxmail.com>
2026-02-08 21:01:21 +0800

    fix:跨域问题

--- e-server/src/main/java/com/ecode/controller/open/CommonController.java
+++ e-server/src/main/java/com/ecode/controller/open/CommonController.java
@@ -74,7 +74,7 @@ public class CommonController {
     }
 
     @PostMapping("/captcha/check")
-    @CrossOrigin(origins = "https://appassets.androidplatform.net")
+    @CrossOrigin(origins = "*")
     public ApiResponse<?> checkCaptcha(@RequestBody Data data) {
         ApiResponse<?> response = imageCaptchaApplication.matching(data.getId(), data.getData());
         if (response.isSuccess()) {
@@ -85,7 +85,7 @@ public class CommonController {
     }
 
     @RequestMapping("/captcha/gen")
-    @CrossOrigin(origins = "https://appassets.androidplatform.net")
+    @CrossOrigin(origins = "*")
     public ApiResponse<ImageCaptchaVO> genCaptcha() {
         // 1.生成验证码(该数据返回给前端用于展示验证码数据)
         // 参数1为具体的验证码类型， 默认支持 SLIDER、ROTATE、WORD_IMAGE_CLICK、CONCAT 等验证码类型，详见： `CaptchaTypeConstant`类
